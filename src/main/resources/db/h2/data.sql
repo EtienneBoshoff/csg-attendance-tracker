d3e4515e9ad6245de1bed8cd187fb69f4efdf439
Etienne Boshoff <boshoff.etienne@gmail.com>
2021-04-24 19:58:36 +0200

    Setting up data scripts

---
+++ src/main/resources/db/h2/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO teacher VALUES (1, 'Grace', 'Masuku');
+
+INSERT INTO class_room VALUES (1, '8-A', 'English', 1);
+
+INSERT INTO student VALUES (1, 'Etienne', 'Boshoff', 1);
+
+INSERT INTO attendance VALUES (1, parsedatetime('24-04-2021 07:18:00', 'dd-MM-yyyy hh:mm:ss'), 'Absent for CLass', 1);
---
+++ src/main/resources/db/h2/schema.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS class_room CASCADE;
+DROP TABLE IF EXISTS student CASCADE;
+DROP TABLE IF EXISTS teacher CASCADE;
+DROP TABLE IF EXISTS attendance CASCADE;
+
+CREATE TABLE teacher (
+    id          INTEGER IDENTITY PRIMARY KEY,
+    first_name  VARCHAR(30),
+    surname     VARCHAR(30)
+);
+CREATE INDEX teacher_surname ON teacher (surname);
+
+CREATE TABLE class_room (
+    id          INTEGER IDENTITY PRIMARY KEY,
+    grade       VARCHAR(10),
+    name        VARCHAR(30),
+    teacher_id  INTEGER NOT NULL
+);
+ALTER TABLE class_room ADD CONSTRAINT fk_teacher_class_room FOREIGN KEY (teacher_id) REFERENCES teacher (id);
+CREATE INDEX class_room_grade ON class_room (grade);
+
+CREATE TABLE student (
+    id          INTEGER IDENTITY PRIMARY KEY,
+    first_name  VARCHAR(30),
+    surname   VARCHAR(30),
+    class_id    INTEGER NOT NULL
+);
+
+ALTER TABLE student ADD CONSTRAINT fk_class_room_student FOREIGN KEY (class_id) REFERENCES class_room (id);
+
+CREATE TABLE attendance (
+    id              INTEGER IDENTITY PRIMARY KEY,
+    created_date    DATE,
+    created_time    TIMESTAMP,
+    status          VARCHAR,
+    student_id      INTEGER NOT NULL
+);
+ALTER TABLE attendance ADD CONSTRAINT fk_student_attendance FOREIGN KEY (student_id) REFERENCES student (id);
+CREATE INDEX attendance_student_created_date ON attendance (student_id, created_date);
+CREATE INDEX attendance_created_date ON attendance (created_date);
+
