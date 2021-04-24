637b031b8f590b7d6aa6e99d01a391ba9d92a531
Etienne Boshoff <boshoff.etienne@gmail.com>
2021-04-24 14:35:01 +0200

    Initial Commit

---
+++ src/test/java/interview/testing/csg/attendancetracker/AttendancetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package interview.testing.csg.attendancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendancetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
