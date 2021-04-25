ab625e2e5abeac11e3f274e02911c0cf43662559
Etienne Boshoff <boshoff.etienne@gmail.com>
2021-04-25 18:57:09 +0200

    Updating functionality

--- src/main/java/interview/testing/csg/attendancetracker/classrooms/ClassRoomRepository.java
+++ src/main/java/interview/testing/csg/attendancetracker/classrooms/ClassRoomRepository.java
@@ -7,7 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 public interface ClassRoomRepository extends Repository<ClassRoom, Integer> {
 
-    @Query("SELECT classRoom FROM ClassRoom classRoom inner join fetch classRoom.teacher WHERE classRoom.teacher.id =:id")
+    @Query("SELECT classRoom FROM ClassRoom classRoom left join fetch classRoom.teacher WHERE classRoom.teacher.id =:id")
     @Transactional(readOnly = true)
     ClassRoom findById(@Param("id") Integer id);
 
